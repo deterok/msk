1112be4ff6230e7fdfac4f147ad1160bdd6439a1
Vadim Suharnikov <vsuharnikov@gmail.com>
2015-06-05 01:06:09 +0300

    feat(base): add support smart-tab-mode

--- base/coding.el
+++ base/coding.el
@@ -2,6 +2,7 @@
                     highlight-parentheses
                     multiple-cursors
                     company
+                    smart-tab-company
                     yasnippet
                     ))
 
@@ -24,8 +25,13 @@
 
 ;;Масштабная система автодополнения кода/текста
 (progn
+
   (require 'company)
   (global-company-mode t)
+
+  (require 'smart-tab)
+  (set-default 'smart-tab-completion-functions-alist '())
+  (global-smart-tab-mode t)
 
   (require 'yasnippet)
   (yas-global-mode t)
@@ -35,7 +41,7 @@
 
   (defun msk/company-backend-with-yas (backend)
     (if (or (not company-mode/enable-yas)
-            (and (listp backend) (member 'company-yasnippet backend)))
+           (and (listp backend) (member 'company-yasnippet backend)))
         backend
       (append (if (consp backend)  backend (list backend))
               '(:with company-yasnippet))))
