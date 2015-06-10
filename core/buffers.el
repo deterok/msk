c59711b3bf94a53246b36aec9897a4b8296d28c7
Vadim Suharnikov <vsuharnikov@gmail.com>
2015-06-10 09:25:35 +0300

    fix(core): fix ibuffer regexps and change groups

--- core/buffers.el
+++ core/buffers.el
@@ -8,16 +8,20 @@
 
 (setq ibuffer-saved-filter-groups
       `(("default"
+         ("Emacs" (or
+                   (name . "^\\*scratch\\*$")
+                   (name . "^\\*Messages\\*$")
+                   (name . "^\\*Compile-Log\\*$")
+                   (name . "^\\*Flycheck")))
+
          ("Terminals" (mode . term-mode))
-         ("emacs.d" (filename . ,(expand-file-name user-emacs-directory)))
-	 ("Help" (or (mode . Man-mode)
-                     (mode . woman-mode)
-                     (mode . Info-mode)
-                     (mode . Help-mode)
-                     (mode . help-mode)))
-         ("Emacs internal" (or (name . "*Messages*")
-                               (name . "*Completions*")
-                               (name . "*Helm log*")
-                               (name . "*helm recentf*")
-                               (name . "*ESS*")
-                               (name . "*Compile-Log*"))))))
+         ("ED" (filename . ,(expand-file-name user-emacs-directory)))
+         ("Debug" (name . "^\\*Backtrace"))
+         ("Help" (or (mode . Man-mode)
+                    (mode . woman-mode)
+                    (mode . Info-mode)
+                    (mode . Help-mode)
+                    (mode . help-mode))))))
+
+(add-hook 'ibuffer-mode-hook
+          (lambda() (ibuffer-switch-to-saved-filter-groups "default")))
