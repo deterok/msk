dc5754c276db04a00e567e7f33704d51839229f6
Vadim Suharnikov <vsuharnikov@gmail.com>
2015-09-20 21:57:10 +0300

    feat(rust): add rust-snippets

--- mods/rust/main.el
+++ mods/rust/main.el
@@ -1,10 +1,12 @@
 (msk/require-pkgs '(rust-mode
                     s
                     racer-mode
-                    flycheck-rust))
+                    flycheck-rust
+                    rust-snippets))
 
 (add-hook 'rust-mode-hook #'racer-mode)
 (add-hook 'racer-mode-hook #'eldoc-mode)
+
 
 (eval-after-load 'flycheck
      '(add-hook 'flycheck-mode-hook #'flycheck-rust-setup))
