d2e54b5f4488b4a8f418b0733d2df7546b5da921
Vadim Suharnikov <vsuharnikov@gmail.com>
2015-09-20 21:38:23 +0300

    feat(rust): add flycheck-rust

--- mods/rust/main.el
+++ mods/rust/main.el
@@ -1,6 +1,10 @@
 (msk/require-pkgs '(rust-mode
                     s
-                    racer-mode))
+                    racer-mode
+                    flycheck-rust))
 
 (add-hook 'rust-mode-hook #'racer-mode)
 (add-hook 'racer-mode-hook #'eldoc-mode)
+
+(eval-after-load 'flycheck
+     '(add-hook 'flycheck-mode-hook #'flycheck-rust-setup))
