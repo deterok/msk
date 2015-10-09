233e7ffd094ca939f13118c17f6f3b7fcf808c9b
Vadim Suharnikov <vsuharnikov@gmail.com>
2015-10-09 11:26:51 +0300

    fix(python): remove obsolete hotkeys

--- mods/python/main.el
+++ mods/python/main.el
@@ -31,11 +31,6 @@
  "';'.join(get_ipython().Completer.all_completions('''%s'''))\n")
 
 
-;; Горячии клавиши для перехода к объявлению объекта и назад
-(define-key anaconda-mode-map (kbd "M-.") 'anaconda-mode-goto)
-(define-key anaconda-mode-map (kbd "M-,") 'anaconda-nav-pop-marker)
-
-
 (defun python-shell-send-buffer-with-main ()
   "Запуск буфера как main модуля"
   (interactive)
