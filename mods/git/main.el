cfdc08139332904fb35653ca9c4bdbdc0eb30a29
Vadim Suharnikov <vsuharnikov@gmail.com>
2015-10-12 09:14:46 +0300

    feat(git): add new default arguments for magit-log

--- mods/git/main.el
+++ mods/git/main.el
@@ -4,6 +4,9 @@
 (add-hook 'magit-status-mode-hook #'magit-filenotify-mode)
 (global-magit-file-mode)
 
+(magit-log-arguments
+    '("--graph" "--color" "--decorate" "--show-signature"))
+
 (progn
   (global-set-key (kbd "<f6>") 'magit-status)
   (global-set-key (kbd "<f7>") 'magit-dispatch-popup))
