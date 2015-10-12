e38352d427522315b8cca836a3a2a0dc4e526e43
Vadim Suharnikov <vsuharnikov@gmail.com>
2015-10-12 09:23:07 +0300

    feat(git): add reset-popup into dispatcher-popup

--- mods/git/main.el
+++ mods/git/main.el
@@ -7,11 +7,37 @@
 (magit-log-arguments
     '("--graph" "--color" "--decorate" "--show-signature"))
 
+(defcustom magit-reset-arguments nil
+  "The arguments used when resetting."
+  :group 'magit-commands
+  :type '(repeat (string :tag "Argument")))
+
+
+(defun magit-reset-popup (&optional arg)
+  "Popup console for reset commands."
+  (interactive "P")
+  (--if-let (magit-commit-message-buffer)
+      (switch-to-buffer it)
+    (magit-invoke-popup 'magit-reset-popup nil arg)))
+
+(defvar magit-reset-popup
+  '(:varible magit-reset-arguments
+    :man-page "git-reset"
+    :actions ((?x "Reset"       magit-reset-index)
+              (?m "Mixed reset" magit-reset)
+              (?s "Soft reset"  magit-reset-soft)
+              (?H "Hard reset"  magit-reset-hard))
+    :max-action-columns 4
+    :default-action magit-reset))
+
 (progn
   (global-set-key (kbd "<f6>") 'magit-status)
   (global-set-key (kbd "<f7>") 'magit-dispatch-popup))
 
 (progn
+  (magit-define-popup-action 'magit-dispatch-popup
+    ?x "Reset" 'magit-reset-popup ?!)
+
   (magit-define-popup-switch 'magit-commit-popup
     ?S "Sign using default gpg" "-S")
 
