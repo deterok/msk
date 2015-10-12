70955f4c66f6b2a4233d1089e559dde525577984
Vadim Suharnikov <vsuharnikov@gmail.com>
2015-10-12 09:18:57 +0300

    feat(git): add new attributs for gpg sign
    Added "-S" for commit-popup and merge-popup
    Adedd "--gpg-sign=" for merge-popup

--- mods/git/main.el
+++ mods/git/main.el
@@ -10,3 +10,13 @@
 (progn
   (global-set-key (kbd "<f6>") 'magit-status)
   (global-set-key (kbd "<f7>") 'magit-dispatch-popup))
+
+(progn
+  (magit-define-popup-switch 'magit-commit-popup
+    ?S "Sign using default gpg" "-S")
+
+  (magit-define-popup-switch 'magit-merge-popup
+    ?S "Sign using default gpg" "-S")
+
+  (magit-define-popup-option 'magit-merge-popup
+    ?S "Sign using gpg" "--gpg-sign="  #'magit-read-gpg-secret-key))
